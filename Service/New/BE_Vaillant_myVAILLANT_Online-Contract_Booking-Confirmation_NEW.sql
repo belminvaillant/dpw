891531567d6a9d462eed07011d32a57b73e8a105
belminabazovic <42625604+belminabazovic@users.noreply.github.com>
2023-07-27 13:00:03 +0200

    Update BE_Vaillant_myVAILLANT_Online-Contract_Booking-Confirmation_NEW.sql

--- Service/New/BE_Vaillant_myVAILLANT_Online-Contract_Booking-Confirmation_NEW.sql
+++ Service/New/BE_Vaillant_myVAILLANT_Online-Contract_Booking-Confirmation_NEW.sql
@@ -60,16 +60,16 @@ acc.Salutation as Salutation,
 acc.FirstName as FirstName,
 acc.LastName as LastName,
 acc.Name as AccountName,
-acc.PersonEmail as Emailadress,
+acc.PersonEmail as Emailadress, 
 acc.AccountNumber as AccountNumber,
-acc.BillingCountry__c as BillingCountry,
-acc.BillingCity__c as BillingCity,
-acc.BillingPostalCode__c as BillingPostalCode,
-acc.BillingStreet__c  as BillingStreet,
-acc.BillingHouseNo__c as BillingHouseNo,
+acc.BillingCountryCode as BillingCountry, /*BillingCountry__c => BillingCountryCode*/
+acc.BillingCity as BillingCity, /*BillingCity__c => BillingCity*/
+acc.BillingPostalCode as BillingPostalCode, /*BillingPostalCode__c => BillingPostalCode*/
+acc.BillingStreet  as BillingStreet, /*BillingStreet__c => BillingStreet*/
+/*acc.BillingHouseNo__c as BillingHouseNo, not needed anymore as HouseNo is included in BillingStreet*/
 acc.BillingFlatNo__c as BillingFlatNo,
 acc.BillingFloor__c as BillingFloor,
-acc.LocaleSidKey__c as PrefLanguage,
+acc.LocaleSidKey__c as PrefLanguage, /*LocaleSidKey__c => TemplateLanguage__c*/
 
 acc2.FirstName as SR_FirstName,
 acc2.LastName as SR_LastName,
@@ -80,14 +80,14 @@ scc.DescriptionInternal__c as Total_Price_EUR,
 scc.MaintenanceDuration__c as ContractDuration, /*tbv*/
 scc.util_MaintenanceDuration__c as ContractDuration_2, /*tbv*/
 scc.util_templatedetails__c as ContractType_NotTranslated,
-scc.Status__c as ContractStatus,
-    scc.StartDate__c as StartDate,
-    datepart(day,scc.StartDate__c) as Start_Day,
-    datepart(month,scc.StartDate__c) as Start_Month,
-    datepart(year,scc.StartDate__c) as Start_Year,
-    datepart(day,getdate()) as Today_D,
-    datepart(month,getdate()) as Today_M,
-    datepart(year,getdate()) as Today_Y,
+scc.Status as ContractStatus, /*Status__c => Status*/
+scc.StartDate__c as StartDate,
+datepart(day,scc.StartDate__c) as Start_Day,
+datepart(month,scc.StartDate__c) as Start_Month,
+datepart(year,scc.StartDate__c) as Start_Year,
+datepart(day,getdate()) as Today_D,
+datepart(month,getdate()) as Today_M,
+datepart(year,getdate()) as Today_Y,
 scc.OrderType__c as OrderType,
 scc.Runtime__c	as RunTime,
 scc.CreatedById as CreatedById,
@@ -97,54 +97,54 @@ scc.LeadCreatorPartner__c as Contract_Lead_Creator_Partner,
 scc.ContractSeller__c as Contract_Salesrep_User,
 scc.AccountOwner__c as Contract_AccountOwner,
 
-ib.InstallationDate__c	as Installatiedatum,
-ib.ProductNameCalc__c as ProductName,
-ib.SerialNo__c	as SerialNumber,
-ib.ProductUnitClass__c as ProductUnitClass,
+asset.InstallDate as Installatiedatum, /*InstallationDate__c => InstallDate*/
+asset.ProductNameCalc__c as ProductName, /*ProductNameCalc__c => Name*/
+asset.SerialNumber as SerialNumber, /*SerialNo__c => SerialNumber*/
+asset.ProductUnitClass__c as ProductUnitClass,
 
-ibl.Country__c as Country,
-ibl.City__c as City,
-ibl.PostalCode__c as PostalCode,
-ibl.Street__c as Street,
-ibl.HouseNo__c as HouseNo,
-ibl.FlatNo__c as FlatNo,
-ibl.Floor__c as Floor_c,
+loc.Country__c as Country,
+loc.City__c as City,
+loc.PostalCode__c as PostalCode,
+loc.Street__c as Street,
+loc.HouseNo__c as HouseNo,
+loc.FlatNo__c as FlatNo,
+loc.Floor__c as Floor_c,
 
 CASE
-when (acc.LocaleSidKey__c = 'fr' and scc.util_templatedetails__c ='Omnium (BE) CSSP')
+when (acc.TemplateLanguage__c = 'fr' and scc.util_templatedetails__c ='Omnium (BE) CSSP') /*LocaleSidKey__c => TemplateLanguage__c*/
 then replace(scc.util_templatedetails__c,'Omnium (BE) CSSP','Omnium')
 
-when (acc.LocaleSidKey__c = 'nl' and scc.util_templatedetails__c = 'Omnium (BE) CSSP')
+when (acc.TemplateLanguage__c = 'nl' and scc.util_templatedetails__c = 'Omnium (BE) CSSP')
 then replace(scc.util_templatedetails__c,'Omnium (BE) CSSP','Omnium')
 
-when (acc.LocaleSidKey__c = 'fr' and scc.util_templatedetails__c = 'Omnium (BE) CSSP - Bruxelles')
+when (acc.TemplateLanguage__c = 'fr' and scc.util_templatedetails__c = 'Omnium (BE) CSSP - Bruxelles')
 then replace(scc.util_templatedetails__c,'Omnium (BE) CSSP - Bruxelles','Omnium')
 
-when (acc.LocaleSidKey__c = 'nl' and scc.util_templatedetails__c = 'Omnium (BE) CSSP - Bruxelles')
+when (acc.TemplateLanguage__c = 'nl' and scc.util_templatedetails__c = 'Omnium (BE) CSSP - Bruxelles')
 then replace(scc.util_templatedetails__c,'Omnium (BE) CSSP - Bruxelles','Omnium')
 
-when (acc.LocaleSidKey__c = 'fr' and scc.util_templatedetails__c = 'Standard (BE) CSSP')
+when (acc.TemplateLanguage__c = 'fr' and scc.util_templatedetails__c = 'Standard (BE) CSSP')
 then replace(scc.util_templatedetails__c,'Standard (BE) CSSP','Standard')
 
-when (acc.LocaleSidKey__c = 'nl' and scc.util_templatedetails__c = 'Standard (BE) CSSP')
+when (acc.TemplateLanguage__c = 'nl' and scc.util_templatedetails__c = 'Standard (BE) CSSP')
 then replace(scc.util_templatedetails__c,'Standard (BE) CSSP','Standaard')
 
-when (acc.LocaleSidKey__c = 'fr' and scc.util_templatedetails__c = 'Standard (BE) CSSP - Bruxelles')
+when (acc.TemplateLanguage__c = 'fr' and scc.util_templatedetails__c = 'Standard (BE) CSSP - Bruxelles')
 then replace(scc.util_templatedetails__c,'Standard (BE) CSSP - Bruxelles','Standard')
 
-when (acc.LocaleSidKey__c = 'nl' and scc.util_templatedetails__c = 'Standard (BE) CSSP - Bruxelles')
+when (acc.TemplateLanguage__c = 'nl' and scc.util_templatedetails__c = 'Standard (BE) CSSP - Bruxelles')
 then replace(scc.util_templatedetails__c,'Standard (BE) CSSP - Bruxelles','Standaard')
 
-when (acc.LocaleSidKey__c = 'fr' and scc.util_templatedetails__c = 'Standard 24 Month CSSP (BE)')
+when (acc.TemplateLanguage__c = 'fr' and scc.util_templatedetails__c = 'Standard 24 Month CSSP (BE)')
 then replace(scc.util_templatedetails__c,'Standard 24 Month CSSP (BE)','Standard')
 
-when (acc.LocaleSidKey__c = 'nl' and scc.util_templatedetails__c = 'Standard 24 Month CSSP (BE)')
+when (acc.TemplateLanguage__c = 'nl' and scc.util_templatedetails__c = 'Standard 24 Month CSSP (BE)')
 then replace(scc.util_templatedetails__c,'Standard 24 Month CSSP (BE)','Standaard')
 
-when (acc.LocaleSidKey__c = 'fr' and scc.util_templatedetails__c = 'Standard 24 Month CSSP (BE) - Bruxelles')
+when (acc.TemplateLanguage__c = 'fr' and scc.util_templatedetails__c = 'Standard 24 Month CSSP (BE) - Bruxelles')
 then replace(scc.util_templatedetails__c,'Standard 24 Month CSSP (BE) - Bruxelles','Standard')
 
-when (acc.LocaleSidKey__c = 'nl' and scc.util_templatedetails__c = 'Standard 24 Month CSSP (BE) - Bruxelles')
+when (acc.TemplateLanguage__c = 'nl' and scc.util_templatedetails__c = 'Standard 24 Month CSSP (BE) - Bruxelles')
 then replace(scc.util_templatedetails__c,'Standard 24 Month CSSP (BE) - Bruxelles','Standaard')
 
 ELSE 'Standard'
@@ -153,14 +153,14 @@ ROW_NUMBER ( ) OVER ( PARTITION BY scc.Id ORDER BY scc.Brand__c ASC ) AS RowNumb
 
 
 FROM ENT.Account_Salesforce_2 acc
-INNER JOIN ENT.SCContract__c_Salesforce_5 scc on scc.AccountOwner__c = acc.id
-INNER JOIN ENT.SCContractItem__c_Salesforce_2 sci on sci.Contract__c = scc.id
-INNER JOIN ENT.SCInstalledBase__c_Salesforce_5 ib on ib.id = sci.InstalledBase__c
-INNER JOIN ENT.SCInstalledBaseLocation__c_Salesforce_3 ibl on ibl.id = ib.installedbaselocation__c
+INNER JOIN ENT.ServiceContract scc on scc.AccountOwner__c = acc.id /*SCContract__c object to ServiceContract */
+INNER JOIN ENT.SCContractItem__c_Salesforce_2 sci on sci.ServiceContractId = scc.id /*SCContractItem__c object to Service Contract Line Item*/
+INNER JOIN ENT.Asset asset on asset.id = sci.AssetId /*SCInstalledBase__c object to Asset*/
+INNER JOIN ENT.Location loc on loc.id = asset.LocationId
 INNER JOIN ENT.Account_Salesforce_2 acc2 on scc.Account__c = acc2.Id
 
-WHERE scc.Status__c = 'suspended'
-and (scc.CreatedById = '005w0000004NnjNAAS' or scc.CreatedById = '005w0000006RcbOAAS')
+WHERE scc.Status = 'suspended' /*Status__c => Status, check values*/
+and (scc.CreatedById = '005w0000004NnjNAAS' or scc.CreatedById = '005w0000006RcbOAAS') /*Two API users*/
 and datepart(day,scc.ConclusionDate__c) = datepart(day,getdate())
 and datepart(month,scc.ConclusionDate__c) = datepart(month,getdate())
 and datepart(year,scc.ConclusionDate__c) = datepart(year,getdate())
