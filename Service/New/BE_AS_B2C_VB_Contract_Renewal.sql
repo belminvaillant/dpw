9622e1267d2044ed2989ddb4a09aa89b47305d04
jasmijnbastiaansen <103104778+jasmijnbastiaansen@users.noreply.github.com>
2023-07-27 17:40:39 +0200

    Update BE_AS_B2C_VB_Contract_Renewal.sql

--- Service/New/BE_AS_B2C_VB_Contract_Renewal.sql
+++ Service/New/BE_AS_B2C_VB_Contract_Renewal.sql
@@ -60,28 +60,16 @@ sr.BillingPostalCode__c as Billing_PostalCode,
 sr.BillingCity__c as Billing_City,
 sr.BillingCountry__c as Billing_Country,
   
-co.Account__c as Contract_AccountId__c,
-co.util_templatedetails__c as ContractDetailTemplate,
-co.Status__c as Contract_Status,
-co.Frame__c as FrameContract,
-co.Brand__c as ContractBrand,
-co.StartDate__c as ContractStartDate__c,
-co.DepartmentCurrent__c as DepartmentBU,
+co.EndDate as Contract_EndDate, /*OK*/
+co.Name as Contract_Name, /*OK*/
+co.ContractNumber as Contract_Number, /*OK*/
+co.TemplateId__c as Contract_Template_Name, /*OK*/
+co.Status as Contract_Status, /*OK*/
+co.Term as Contract_Term_Months, /*OK*/
+  
+pb.Brand__c as Brand, /*OK*/
 
-pb.Brand__c as Brand,
-  
-cov.Status__c as ContractVisit_Status__c,
-cov.Id as ContractVisitId__c,
-cov.Contract__c as ContractVisit_Contract__c,
-cov.DueDate__c as Contract_VisitDueDate,
-cov.OriginalDueDate__c as ContractVisit_OriginalDueDate,
-  
-coi.Contract__c as ContractItemContract__c,
-coi.util_ib_product_model__c as ProductModel__c,
-coi.InstalledBase__c as CV_InstalledBase_Id,
-  
-ins.Id as InstalledBase_Id,
-ins.ProductUnitClass__c as ProductUnitClass,
+prod.Name as Product_Name /*Which product name to put in the email?*/
   
 ROW_NUMBER ( ) OVER ( PARTITION BY acc.PersonContactId ORDER BY acc.Name ASC ) AS RowNumber
 
@@ -90,6 +78,11 @@ FROM ENT.ServiceContract_Salesforce_4 co
 INNER JOIN ENT.Account_Salesforce_21 sr on acc.Id = co.Service_Recipient__c
 INNER JOIN ENT.Account_Salesforce_21 ir on acc.Id = co.Invoice_Recipient__c
 INNER JOIN ENT.Pricebook2_Salesforce pb on pb.Id = co.Pricebook2Id
+INNER JOIN ENT.MaintenancePlan_Salesforce mp on co.Id = mp.ServiceContractId
+INNER JOIN ENT.MaintenanceAsset_Salesforce mass on mass.MaintenancePlanId = mp.Id
+INNER JOIN ENT.Asset_Salesforce_5 ass on mp.AssetId = ass.Id
+INNER JOIN ENT.Product2 prod on prod.id = ass.Product2Id
+/*INNER JOIN ENT.WorkOrder_Salesforce wo on wo. Needed? */
 
 WHERE
 ir.DeleteStatus__c IS NULL  /*OK*/
@@ -99,15 +92,11 @@ AND ir.Email__c <> '' /*OK*/
 AND co.TemplateCountry__c = 'BE' /*OK*/
 AND co.Status = 'Active' /*OK*/
 AND co.FSL_Cancellation_Status__c	= '' /*OK*/
-AND co.EndDate = DateAdd(NOW(),m,2) /*of is het FSL_Tentative_End_Date__c?*/
+AND co.EndDate = DateAdd(NOW(),m,2)
 AND co.TemplateId__c (Frame and Bulk)
 AND co.FSL_Type_of_Contract__c /* Needed? */  
-
+/* Do we need to exclude certain types of products?*/
   
-/*AND co.util_templatedetails__c NOT LIKE '%Extended Warranty%'*/
-/*AND (ins.ProductUnitClass__c != '08' AND ins.ProductUnitClass__c != '07' AND ins.ProductUnitClass__c != '03' AND ins.ProductUnitClass__c != '10')
-/*AND (coi.util_ib_product_model__c NOT LIKE '%Other%' AND coi.util_ib_product_model__c != 'turboMAG /1 plus [145]' AND coi.util_ib_product_model__c != 'turboMAG /1 plus [115]' AND coi.util_ib_product_model__c != 'turboMAG /1 plus [175]' AND coi.util_ib_product_model__c != 'VC ecoTEC /5-5 plus (80-120kW) [806]' AND coi.util_ib_product_model__c != 'VC ecoTEC /5-5 plus (80-120kW) [1206]' AND coi.util_ib_product_model__c != 'VC ecoTEC /5-5 plus (80-120kW) [1006]' AND coi.util_ib_product_model__c != 'VKK ecoCRAFT /3 [1206]' AND coi.util_ib_product_model__c != 'VKK ecoCRAFT /3 [1606]' AND coi.util_ib_product_model__c != 'VKK ecoCRAFT /3 [2006]' AND coi.util_ib_product_model__c != 'VKK ecoCRAFT /3 [2406]' AND coi.util_ib_product_model__c != 'VKK ecoCRAFT /3 [2806]' AND coi.util_ib_product_model__c != 'VKK ecoCRAFT /3 [806]' AND coi.util_ib_product_model__c != 'VU ecoTEC /5-7 exclusive [186]' AND coi.util_ib_product_model__c != 'VU ecoTEC /5-7 exclusive [246]' AND coi.util_ib_product_model__c != 'VU ecoTEC /5-7 exclusive [306]' AND coi.util_ib_product_model__c != 'VUW ecoTEC /5-7 exclusive [356]' AND coi.util_ib_product_model__c != 'VUW ecoTEC /5-7 exclusive [436]' AND coi.util_ib_product_model__c != 'VC ecoTEC /8-5 plus (loni) [206]')*/
-/*AND NOT EXISTS (Select 1 FROM BE_Vaillant_CSSP_Maintenance_Pilot be WHERE be.Person_Account_Id = acc.PersonContactId)
 )
 AS Sub
 WHERE RowNumber=1
