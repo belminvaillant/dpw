a13d76f06c43292b9b68e9e2e39de0b002a8aa63
belminabazovic <42625604+belminabazovic@users.noreply.github.com>
2023-08-07 13:28:04 +0200

    07-08

--- Service/New/BE_AS_B2C_VB_Service_App_Reminder_1Day.sql
+++ Service/New/BE_AS_B2C_VB_Service_App_Reminder_1Day.sql
@@ -5,12 +5,12 @@ Appointment_StartDay,
 Appointment_StartTime,
 Appointment_EndTime,
 CASE
-    WHEN (Recipient_Language = 'NL' AND (Appointment_StartTime >= '00:00' AND Appointment_StartTime <= '10:59')) THEN '07u30 en 12u'
-    WHEN (Recipient_Language = 'NL' AND (Appointment_StartTime >= '11:00' AND Appointment_StartTime <= '12:59')) THEN '10u en 14u'
-    WHEN (Recipient_Language = 'NL' AND (Appointment_StartTime >= '13:00' AND Appointment_StartTime <= '23:59')) THEN '13u en 18u'
-    WHEN (Recipient_Language = 'FR' AND (Appointment_StartTime >= '00:00' AND Appointment_StartTime <= '10:59')) THEN '07h30 et 12h'
-    WHEN (Recipient_Language = 'FR' AND (Appointment_StartTime >= '11:00' AND Appointment_StartTime <= '12:59')) THEN '10h et 14h'
-    WHEN (Recipient_Language = 'FR' AND (Appointment_StartTime >= '13:00' AND Appointment_StartTime <= '23:59')) THEN '13h et 18h'
+    WHEN (SR_Language = 'NL' AND (Appointment_StartTime >= '00:00' AND Appointment_StartTime <= '10:59')) THEN '07u30 en 12u'
+    WHEN (SR_Language = 'NL' AND (Appointment_StartTime >= '11:00' AND Appointment_StartTime <= '12:59')) THEN '10u en 14u'
+    WHEN (SR_Language = 'NL' AND (Appointment_StartTime >= '13:00' AND Appointment_StartTime <= '23:59')) THEN '13u en 18u'
+    WHEN (SR_Language = 'FR' AND (Appointment_StartTime >= '00:00' AND Appointment_StartTime <= '10:59')) THEN '07h30 et 12h'
+    WHEN (SR_Language = 'FR' AND (Appointment_StartTime >= '11:00' AND Appointment_StartTime <= '12:59')) THEN '10h et 14h'
+    WHEN (SR_Language = 'FR' AND (Appointment_StartTime >= '13:00' AND Appointment_StartTime <= '23:59')) THEN '13h et 18h'
     ELSE Appointment_StartTime
 END as CommunicatedTimeWindow,
 Appointment_Status, 
@@ -22,20 +22,21 @@ Appointment_WorkTypeId,
 Appointment_Number,
 
 Order_Id,
-Order_AccountId,
+Order_SR,
+Order_IR,
 Order_ContractId,
 Order_Status,
 
 Contract_Id,
 
-Recipient_Id,
-Recipient_PersonAccount,
-Recipient_FirstName,
-Recipient_LastName,
-Recipient_Email, 
-Recipient_AccountNumber,
-Recipient_Language,
-Recipient_MobilePhone,
+SR_Id,
+SR_PersonAccount,
+SR_FirstName,
+SR_LastName,
+SR_Email, 
+SR_AccountNumber,
+SR_Language,
+SR_MobilePhone,
 
 Asset_Brand,
 
@@ -58,43 +59,39 @@ app.WorkTypeId as Appointment_WorkTypeId,
 app.AppointmentNumber as Appointment_Number,
 
 ord.Id as Order_Id,
-ord.AccountId as Order_AccountId,
+ord.AccountId as Order_SR,
+ord.FSL_Invoice_Recipient__c as Order_IR,
 ord.ServiceContractId as Order_ContractId,
 ord.Status as Order_Status,
 
 con.Id as Contract_Id,
 con.FrameReferenceNumber__c as Contract_Frame,
-/*
 
-CASE 
-    WHEN (con.Frame__c IN ('a0C1r00003pBl9xEAC', 'a0C1r00003pBn8fEAC', 'a0C1r00003pBnc1EAC', 'a0C1r00003pAp8fEAC', 'a0C1r000042mBBuEAM', 'a0C6900004FDg8cEAD', 'a0C1r000043BCxWEAW', 'a0C1r00004AWdHDEA1', 'a0C69000048JyyFEAS', 'a0C6900004NgIJoEAN', 'a0C1r00003w47NVEAY', 'a0Cw000001GNm6qEAD', 'a0C6900004O0WqtEAF', 'a0C6900004DiveKEAR', 'a0C1r00003xRltxEAC')) THEN 'No'
-    ELSE 'Yes'
-END AS InScope,*/
-
-acc.Id as Recipient_Id,
-acc.IsPersonAccount as Recipient_PersonAccount,
-acc.FirstName as Recipient_FirstName,
-acc.LastName as Recipient_LastName,
-acc.PersonEmail as Recipient_Email, 
-acc.AccountNumber as Recipient_AccountNumber,
-acc.TemplateLanguage__c as Recipient_Language,
-acc.PersonMobilePhone as Recipient_MobilePhone,
+sr.Id as SR_Id,
+sr.IsPersonAccount as SR_PersonAccount,
+sr.FirstName as SR_FirstName,
+sr.LastName as SR_LastName,
+sr.PersonEmail as SR_Email, 
+sr.AccountNumber as SR_AccountNumber,
+sr.TemplateLanguage__c as SR_Language,
+sr.PersonMobilePhone as SR_MobilePhone,
 
 asset.Brand__c as Asset_Brand,
 
-ROW_NUMBER () OVER (PARTITION BY acc.Id ORDER BY app.FSL_Scheduled_Start__c ASC ) AS RowNumber
+ROW_NUMBER () OVER (PARTITION BY sr.Id ORDER BY app.FSL_Scheduled_Start__c ASC ) AS RowNumber
 
 FROM ENT.WorkOrder_Salesforce ord
 INNER JOIN ENT.ServiceAppointment_Salesforce app on ord.Id = app.FSL_Work_Order__c
 LEFT JOIN ENT.ServiceContract_Salesforce_4 con on ord.ServiceContractId = con.Id
-LEFT JOIN ENT.Account_Salesforce_21 acc on ord.AccountId = acc.Id
+LEFT JOIN ENT.Account_Salesforce_21 sr on ord.AccountId = sr.Id
+LEFT JOIN ENT.Account_Salesforce_21 ir on ord.FSL_Invoice_Recipient__c = ir.Id
 LEFT JOIN ENT.Asset_Salesforce_5 asset on asset.Id = ord.AssetId 
 
 WHERE
 convert(int,dateadd(day,convert(float,getdate()), 1))=convert(int,app.FSL_Scheduled_Start__c)
 AND app.Country = 'Belgium'
 AND ord.Status = 'In Progress'
-AND acc.TemplateLanguage__c IN ('FR', 'NL')
+AND sr.TemplateLanguage__c IN ('FR', 'NL') /*Only for testing purposes*/
 AND app.Status != 'Cancelled'
 
 ORDER BY app.ActualStartTime ASC
--- Service/New/BE_Vaillant_myVAILLANT_Online-Contract_Booking-Confirmation_NEW.sql
+++ Service/New/BE_Vaillant_myVAILLANT_Online-Contract_Booking-Confirmation_NEW.sql
@@ -38,7 +38,7 @@ Contract_Id,
 Contract_Lead_Creator_Partner,
 Contract_Salesrep_User,
 Contract_AccountOwner,
-Installatiedatum,
+InstallDate,
 ProductName,
 SerialNumber,
 ProductUnitClass,
@@ -52,7 +52,7 @@ Floor_c
 
 FROM(
 SELECT
-acc.id as SubscriberKey,
+acc.Id as SubscriberKey,
 acc.PersonContactId as Subscriberkey2,
 acc.Salutation as Salutation,
 acc.FirstName as FirstName,
@@ -60,43 +60,42 @@ acc.LastName as LastName,
 acc.Name as AccountName,
 acc.PersonEmail as Emailadress, 
 acc.AccountNumber as AccountNumber,
-acc.BillingCountryCode as BillingCountry, /*BillingCountry__c => BillingCountryCode*/
-acc.BillingCity as BillingCity, /*BillingCity__c => BillingCity*/
-acc.BillingPostalCode as BillingPostalCode, /*BillingPostalCode__c => BillingPostalCode*/
-acc.BillingStreet  as BillingStreet, /*BillingStreet__c => BillingStreet*/
-/*acc.BillingHouseNo__c as BillingHouseNo, not needed anymore as HouseNo is included in BillingStreet*/
+acc.BillingCountryCode as BillingCountry,
+acc.BillingCity as BillingCity,
+acc.BillingPostalCode as BillingPostalCode,
+acc.BillingStreet  as BillingStreet,
 acc.BillingFlatNo__c as BillingFlatNo,
 acc.BillingFloor__c as BillingFloor,
-acc.LocaleSidKey__c as PrefLanguage, /*LocaleSidKey__c => TemplateLanguage__c*/
+acc.TemplateLanguage__c as Language,
 
 acc2.FirstName as SR_FirstName,
 acc2.LastName as SR_LastName,
 
-scc.Brand__c as Brand, /*Doesn"t exist in FSL, take from Asset*/
-scc.DescriptionSpecialTerms__c as VAT_Value_Percent,
-scc.DescriptionInternal__c as Total_Price_EUR,
+/*scc.DescriptionSpecialTerms__c as VAT_Value_Percent,
+scc.DescriptionInternal__c as Total_Price_EUR,*/
 scc.Name as ContractType_NotTranslated, 
-scc.Status as ContractStatus, /*Status__c => Status*/
-scc.FSL_Tentative_Start_Date__c as StartDate, /*StartDate__c => FSL_Tentative_Start_Date__c*/
-datepart(day,scc.FSL_Tentative_Start_Date__c) as Start_Day, /*StartDate__c => FSL_Tentative_Start_Date__c*/
-datepart(month,scc.FSL_Tentative_Start_Date__c) as Start_Month, /*StartDate__c => FSL_Tentative_Start_Date__c*/
-datepart(year,scc.FSL_Tentative_Start_Date__c) as Start_Year, /*StartDate__c => FSL_Tentative_Start_Date__c*/
+scc.Status as ContractStatus,
+scc.FSL_Tentative_Start_Date__c as StartDate,
+datepart(day,scc.FSL_Tentative_Start_Date__c) as Start_Day,
+datepart(month,scc.FSL_Tentative_Start_Date__c) as Start_Month, 
+datepart(year,scc.FSL_Tentative_Start_Date__c) as Start_Year,
 datepart(day,getdate()) as Today_D,
 datepart(month,getdate()) as Today_M,
 datepart(year,getdate()) as Today_Y,
 scc.OrderType__c as OrderType,
-scc.Term as RunTime, /*RunTime__c => Term*/
+scc.Term as RunTime,
 scc.CreatedById as CreatedById,
 scc.ConclusionDate__c as ContractCreatedDate,
 scc.Id as Contract_Id,
-scc.LeadCreatorPartner__c as Contract_Lead_Creator_Partner,
-scc.ContractSeller__c as Contract_Salesrep_User,
-scc.AccountId as Contract_AccountOwner,
+/*scc.LeadCreatorPartner__c as Contract_Lead_Creator_Partner,*/
+/*scc.ContractSeller__c as Contract_Salesrep_User,*/
+scc.Invoice_Recipient__c as Contract_AccountOwner,
 
-asset.InstallDate as Installatiedatum, /*InstallationDate__c => InstallDate*/
-asset.ProductNameCalc__c as ProductName, /*ProductNameCalc__c => Name*/
-asset.SerialNumber as SerialNumber, /*SerialNo__c => SerialNumber*/
+asset.InstallDate as InstallDate, 
+asset.Name as ProductName,
+asset.SerialNumber as SerialNumber
 asset.ProductUnitClass__c as ProductUnitClass,
+asset.Brand__c as Brand,
 
 loc.Country__c as Country,
 loc.City__c as City,
@@ -110,14 +109,15 @@ ROW_NUMBER ( ) OVER ( PARTITION BY scc.Id ORDER BY scc.Brand__c ASC ) AS RowNumb
 
 
 FROM ENT.Account_Salesforce_2 acc
-INNER JOIN ENT.ServiceContract_Salesforce_4 scc on scc.AccountId = acc.id /*SCContract__c object to ServiceContract */
-INNER JOIN ENT.Asset_Salesforce_5 asset on asset.id = scc.AssetId__c
+INNER JOIN ENT.ServiceContract_Salesforce_4 scc on scc.AccountId = acc.id
+INNER JOIN ENT.ContractLineItem_Salesforce cli on cli.ServiceContractId = scc.id
+INNER JOIN ENT.Asset_Salesforce_5 asset on asset.id = cli.AssetId
 INNER JOIN ENT.Location_Salesforce loc on loc.id = asset.LocationId
-INNER JOIN ENT.Account_Salesforce_2 acc2 on scc.Account__c = acc2.Id
+INNER JOIN ENT.Account_Salesforce_2 acc2 on scc.Service_Recipient__c = acc2.Id
 
 WHERE scc.Status = 'suspended' /*Status__c => Status, new values are "Inactive", "Active", "Expired"*/
 and (scc.CreatedById = '005w0000004NnjNAAS' or scc.CreatedById = '005w0000006RcbOAAS')
-and datepart(day,scc.ConclusionDate__c) = datepart(day,getdate())
+and datepart(day,scc.Crea) = datepart(day,getdate())
 and datepart(month,scc.ConclusionDate__c) = datepart(month,getdate())
 and datepart(year,scc.ConclusionDate__c) = datepart(year,getdate())
 /*and scc.LeadCreatorPartner__c is null
